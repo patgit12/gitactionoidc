57d730420f16b1da9b7da36ddac7d34e846520fd
patongit <90884766+patgit12@users.noreply.github.com>
2023-10-17 23:40:42 -0400

    initial commit

---
+++ environments/s3/s3_backend.tf
@@ -0,0 +1,24 @@
+module "callbackend" {
+  source = "git@github.com:patgit12/s3backend.git//s3_module/?ref=v1.2.1"
+  }
+
+/* #Backend configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "landmark-automation-kenmak"
+    region  = "us-west-2"
+    key     = "s3/terraform.tfstate"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+} */
---
+++ oidc.tf
@@ -0,0 +1,67 @@
+data "tls_certificate" "eks" {
+  url = "https://token.actions.githubusercontent.com"
+
+}
+resource "aws_iam_openid_connect_provider" "git" {
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [ data.tls_certificate.eks[0].sha1_fingerprint ]
+  url = "https://token.actions.githubusercontent.com"
+}
+
+############ Role to be assumed with a federated pricipal  #############
+data "aws_iam_policy_document" "git_aws_oidc" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect = "Allow"
+
+    condition {
+      test = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect.git.url, "https://", "")}:aud"
+      values = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect.git.url, "https://", "")}:sub"
+      values = ["repo/*:*"]
+    }
+
+    principals {
+      identifiers = [ aws_iam_openid_connect_provider.git.arn ]
+      type = "Federated"
+    }
+  }
+
+}
+
+resource "aws_iam_role" "git_action" {
+  assume_role_policy = data.aws_iam_policy_document.git_aws_oidc.json
+  name = "git-actions-oidc"
+} 
+
+
+resource "aws_iam_policy" "git_action" {
+  name = "git-actions-oidc"
+  policy = jsonencode( {
+    Statement = [ {
+      Action  = ["*"]
+      Effect  = "Allow"
+      Resource = "*"
+    }]
+
+    Version = "2012-10-17"
+  } )
+  
+}
+
+resource "aws_iam_role_policy_attachment" "git_actions_oidc_attachment" {
+  role = aws_iam_role.git_action.name
+  policy_arn = aws_iam_policy.git_action.arn
+    
+}
+output "git_actions_oidc" {
+  value = aws_iam_role.git_action.arn
+  
+}
+
+provider "aws" {}
